1b5d62944a2d728ea6207d08ae66ab9265d737c8
Pujan Dahal <pujan.dahal@fusemachines.com>
2022-09-06 13:37:07 +0545

    first commit

---
+++ task1.py
@@ -0,0 +1,63 @@
+#getting started
+print("Hello World!")
+
+# Python Keywords and Identifiers
+# Python Statements, Indentation,Comments
+apples = 4
+price_per_apple = 20
+total_cost_of_apples = apples * price_per_apple
+print("Total cost of apples is: ", total_cost_of_apples)
+
+letter_to_friend = "Hello friend, it's been long since we met each other"
+print(letter_to_friend)
+first_part_of_letter = letter_to_friend[:18]
+print(first_part_of_letter)
+
+is_male = False
+if is_male:
+    print("You are a guy.")
+else:
+    print("You are a girl.")
+
+# We can write single line comments using # at the beginning of statement
+"""
+Multi line comments are written using """ """ quotes.
+It's fun to learn python.
+The line can extend how much ever we want.
+"""
+
+
+"""
+Indentation is the key in python as we do not have opening and closing brackets
+like C, C++, Javascript. So code within a code block like function, if..else
+construct is defined by indenting within the code block
+"""
+
+def add_a_b(a, b):
+    #here indentation needs to be used to enclose below lines into the function block
+    result = a + b #local variable result, this line is also a statement
+    return result
+
+
+# Docstring
+# Python Variable, constant and literal
+# Python Data Type
+# Python Type Conversion and Type casting
+# Python input, output and import
+# Python Operators
+# Python namespace
+# Python Flow Control ( if else )
+# Python Loop (for, while)
+# Python break, continue and pass
+# Functions
+# Return statement
+# Function Arguments (Default, keyword, arbitrary)
+# Inbuilt functions
+# Python Anonymous/ Lambda Function
+# Filters, map functions
+# Local, Global variables
+# Python Data Types in details (List, Dictionary, Tuple, Set, Strings)
+
+
+
+
